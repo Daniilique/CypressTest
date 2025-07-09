be14308b31fc50185ca59a9d49adc40d2236a481
Daniil Dmytriiev <daniildtd@gmail.com>
2025-07-09 14:25:56 +0300

    Fixed the link and added login functionality with fixtured data

--- cypress/e2e/main.cy.js
+++ cypress/e2e/main.cy.js
@@ -1,5 +1,20 @@
-describe('template spec', () => {
+describe('Login Page - fixture test user', () => {
+  let users;
+
+  before(() => {
+    cy.fixture('users').then((fixtureData) => {
+      users = fixtureData;
+    });
+  });
+
+
   it('passes', () => {
-    cy.visit('hhttps://www.saucedemo.com/')
+    cy.visit('https://www.saucedemo.com/')
+    const validUser = users.validUser;
+
+    cy.get('#user-name').type(validUser.username);
+    cy.get('#password').type(validUser.password);
+    cy.get('#login-button').click();
+
   })
 })
