2f84db1e6ce115fb1f1b1149f2ec929bffed1163
Daniil Dmytriiev <daniildtd@gmail.com>
2025-07-09 14:58:58 +0300

    Added navigating to site each time the test will start, also added comments

--- cypress/e2e/main.cy.js
+++ cypress/e2e/main.cy.js
@@ -1,22 +1,27 @@
 describe('Login Page - fixture test user', () => {
   let users;
 
+  // Load fixture before test begins
   before(() => {
     cy.fixture('users').then((fixtureData) => {
       users = fixtureData;
     });
   });
 
+  // navigate to the login page before each test
+  beforeEach(() => {
+    cy.visit('https://www.saucedemo.com/');
+  });
 
-  it('passes', () => {
-    cy.visit('https://www.saucedemo.com/')
+  it('Should login succesfuly with corect credentials', () => {
     const validUser = users.validUser;
 
+    //Fill the data into the forms and click login
     cy.get('#user-name').type(validUser.username);
     cy.get('#password').type(validUser.password);
     cy.get('#login-button').click();
 
+    // Verify successful login
     cy.url().should('include', '/inventory.html'); 
-
-  })
-})
+  });
+});
